2b356ddebd68f3b8ce633ae876441e23dcb9f126
Denis Defreyne <git@denisdefreyne.com>
2023-02-04 10:48:12 +0100

    Cleanup

--- lib/deli.rb
+++ lib/deli.rb
@@ -1,8 +1,14 @@
+require "strscan"
+
+# Utilities
 require_relative "deli/source_code"
 require_relative "deli/error"
+
+# Primary models
 require_relative "deli/token"
 require_relative "deli/ast"
 
+# Services
 require_relative "deli/lexer"
 require_relative "deli/parser"
 require_relative "deli/evaluator"
