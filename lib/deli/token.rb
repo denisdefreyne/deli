39d9f666957ce6ff7ebe0c51df03e908c0e9a136
Denis Defreyne <git@denisdefreyne.com>
2023-02-06 19:55:13 +0100

    Convert Token into regular class

--- lib/deli/token.rb
+++ lib/deli/token.rb
@@ -1,9 +1,27 @@
 # frozen_string_literal: true
 
 module Deli
-  Token = Struct.new(:type, :lexeme, :value, :span) do
+  class Token
+    attr_reader :type
+    attr_reader :lexeme
+    attr_reader :value
+    attr_reader :span
+
+    def initialize(type:, lexeme:, value:, span:)
+      @type   = type
+      @lexeme = lexeme
+      @value  = value
+      @span   = span
+    end
+
     def inspect
-      format('%-10<type>s  %<value>s', {type:, value: value ? value.inspect : nil}).strip
+      format(
+        '%-10<type>s  %<value>s',
+        {
+          type:,
+          value: value&.inspect,
+        },
+      ).strip
     end
   end
 end
