475e30e1895b922a8e18946adbc903200362e277
Denis Defreyne <git@denisdefreyne.com>
2023-02-06 10:09:15 +0100

    Test numbers

--- test/deli/lexer_test.rb
+++ test/deli/lexer_test.rb
@@ -3,18 +3,22 @@
 require 'test_helper'
 
 class TestDeliLexer < Minitest::Test
-  def test_binary_operators
-    tokens = lex('1 + 2 - 3 * 4 / 5')
+  def test_numbers
+    tokens = lex('0 1 123')
 
-    assert_token(:NUMBER,   '1', '1', tokens.shift)
+    assert_token(:NUMBER, '0',   '0', tokens.shift)
+    assert_token(:NUMBER, '1',   '1', tokens.shift)
+    assert_token(:NUMBER, '123', '123', tokens.shift)
+    assert_nil(tokens.shift)
+  end
+
+  def test_binary_operators
+    tokens = lex('+ - * /')
+
     assert_token(:PLUS,     '+', nil, tokens.shift)
-    assert_token(:NUMBER,   '2', '2', tokens.shift)
     assert_token(:MINUS,    '-', nil, tokens.shift)
-    assert_token(:NUMBER,   '3', '3', tokens.shift)
     assert_token(:ASTERISK, '*', nil, tokens.shift)
-    assert_token(:NUMBER,   '4', '4', tokens.shift)
     assert_token(:SLASH,    '/', nil, tokens.shift)
-    assert_token(:NUMBER,   '5', '5', tokens.shift)
     assert_nil(tokens.shift)
   end
 
