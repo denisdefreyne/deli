74294d93fcdaffb7e94ac3b858ef799dfc6ab9ab
Denis Defreyne <git@denisdefreyne.com>
2023-02-11 10:15:24 +0100

    Add basic lexer mode stack

--- lib/deli/lexer.rb
+++ lib/deli/lexer.rb
@@ -3,8 +3,9 @@
 module Deli
   class Lexer
     def initialize(source_code)
-      @source_code = source_code
       @scanner = StringScanner.new(source_code.string)
+      mode = MainLexerMode.new(source_code, @scanner)
+      @mode_stack = [mode]
     end
 
     def call
@@ -15,74 +16,105 @@ module Deli
 
       # Add EOF token
       @scanner.scan('')
-      tokens << new_token(TokenType::EOF)
+      tokens << @mode_stack.last.new_token(TokenType::EOF)
 
       tokens
     end
 
-    private
-
     def lex_token
-      if @scanner.eos?
+      mode = @mode_stack.last
+      mode.lex_token(@mode_stack)
+    end
+  end
+
+  class AbstractLexerMode
+    attr_reader :scanner
+
+    def initialize(source_code, scanner)
+      @source_code = source_code
+      @scanner = scanner
+    end
+
+    def span
+      Span.new(
+        @source_code.filename,
+        @scanner.charpos - @scanner.matched.length,
+        @scanner.matched.length,
+      )
+    end
+
+    def new_token(type, value = nil)
+      Token.new(
+        type: type,
+        lexeme: @scanner.matched,
+        value: value,
+        span: span,
+      )
+    end
+  end
+
+  class MainLexerMode < AbstractLexerMode
+    def lex_token(mode_stack)
+      if scanner.eos?
         nil
 
       # Whitespace
-      elsif @scanner.scan(/\s+/)
-        lex_token
+      elsif scanner.scan(/\s+/)
+        lex_token(mode_stack)
 
       # Two-character tokens
-      elsif @scanner.scan('==')
+      elsif scanner.scan('==')
         new_token(TokenType::EQ_EQ)
-      elsif @scanner.scan('!=')
+      elsif scanner.scan('!=')
         new_token(TokenType::BANG_EQ)
-      elsif @scanner.scan('<=')
+      elsif scanner.scan('<=')
         new_token(TokenType::LTE)
-      elsif @scanner.scan('>=')
+      elsif scanner.scan('>=')
         new_token(TokenType::GTE)
 
       # One-character tokens
-      elsif @scanner.scan('+')
+      elsif scanner.scan('+')
         new_token(TokenType::PLUS)
-      elsif @scanner.scan('-')
+      elsif scanner.scan('-')
         new_token(TokenType::MINUS)
-      elsif @scanner.scan('*')
+      elsif scanner.scan('*')
         new_token(TokenType::ASTERISK)
-      elsif @scanner.scan('/')
+      elsif scanner.scan('/')
         new_token(TokenType::SLASH)
-      elsif @scanner.scan('<')
+      elsif scanner.scan('<')
         new_token(TokenType::LT)
-      elsif @scanner.scan('>')
+      elsif scanner.scan('>')
         new_token(TokenType::GT)
-      elsif @scanner.scan(',')
+      elsif scanner.scan(',')
         new_token(TokenType::COMMA)
-      elsif @scanner.scan(';')
+      elsif scanner.scan(';')
         new_token(TokenType::SEMICOLON)
-      elsif @scanner.scan('=')
+      elsif scanner.scan('=')
         new_token(TokenType::EQ)
-      elsif @scanner.scan('!')
+      elsif scanner.scan('!')
         new_token(TokenType::BANG)
-      elsif @scanner.scan('(')
+      elsif scanner.scan('(')
         new_token(TokenType::LPAREN)
-      elsif @scanner.scan(')')
+      elsif scanner.scan(')')
         new_token(TokenType::RPAREN)
-      elsif @scanner.scan('{')
+      elsif scanner.scan('{')
         new_token(TokenType::LBRACE)
-      elsif @scanner.scan('}')
+      elsif scanner.scan('}')
         new_token(TokenType::RBRACE)
-      elsif @scanner.scan('[')
+      elsif scanner.scan('[')
         new_token(TokenType::LBRACKET)
-      elsif @scanner.scan(']')
+      elsif scanner.scan(']')
         new_token(TokenType::RBRACKET)
 
       # String
-      elsif @scanner.scan(/"[^"]+"/)
-        new_token(TokenType::STRING, @scanner.matched[1..-2])
+      elsif scanner.scan(/"[^"]+"/)
+        new_token(TokenType::STRING, scanner.matched[1..-2])
 
       # Values
-      elsif @scanner.scan(/\d+/)
-        new_token(TokenType::NUMBER, @scanner.matched)
-      elsif @scanner.scan(/\w+/)
-        case @scanner.matched
+      elsif scanner.scan(/\d+/)
+        new_token(TokenType::NUMBER, scanner.matched)
+      elsif scanner.scan(/\w+/)
+        case scanner.matched
 
         # Keywords
         when 'true'
@@ -110,32 +142,15 @@ module Deli
 
         # Identifier
         else
-          new_token(TokenType::IDENT, @scanner.matched)
+          new_token(TokenType::IDENT, scanner.matched)
         end
       else
-        char = @scanner.getch
+        char = scanner.getch
         raise Deli::LocatableError.new(
           "Unknown character: #{char}",
           span,
         )
       end
-    end
-
-    def span
-      Span.new(
-        @source_code.filename,
-        @scanner.charpos - @scanner.matched.length,
-        @scanner.matched.length,
-      )
-    end
-
-    def new_token(type, value = nil)
-      Token.new(
-        type: type,
-        lexeme: @scanner.matched,
-        value: value,
-        span: span,
-      )
     end
   end
 end
